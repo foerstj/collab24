b358b043ec902bda6ce2142f5a123b5ceea5f518
Johannes Förstner <johannes@famfoerstner.de>
2024-07-21 15:17:37 +0200

    Official start pos in town

--- build.bat
+++ build.bat
@@ -38,6 +38,10 @@ popd
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /S
+pushd %gaspy%
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+if %errorlevel% neq 0 pause
+popd
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
